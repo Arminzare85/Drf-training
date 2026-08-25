cae4288ccb807c8c791fc0f69c479dabcaae37dd
Arminzare85 <shamino85xrmxn@gmail.com>
2026-08-25 10:28:02 +0330

    using APIView for CRUD and list view

--- core/Blog/api/v1/views.py
+++ core/Blog/api/v1/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated ,IsAuthenticatedOrReadOnl
 from .serializers import PostSerializer
 from ...models import Post
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
+
 
 # Create your views here.
 # @api_view(['GET', 'POST'])
@@ -45,35 +47,17 @@ from rest_framework.views import APIView
 
 
 """ CBV to showing posts """
-class PostList(APIView):
-    def get(self, request):
-        posts = Post.objects.filter(status=True)
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
 
-class PostDetail(APIView):
-    def get(self, request, id):
-        post = Post.objects.get(id=id)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    def put(self, request, id):
-        post = Post.objects.get(id=id)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-    def delete(self, request, id):
-        post = Post.objects.get(id=id)
-        post.delete()
-        return Response("Post deleted" , status=204)
+class PostDetail(RetrieveUpdateDestroyAPIView ):
+
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
         
