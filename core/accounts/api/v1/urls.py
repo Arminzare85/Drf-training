2ced719d5d2e3cb7f9bbcb9b6ee8de5660271c72
Arminzare85 <shamino85xrmxn@gmail.com>
2026-08-31 15:29:56 +0330

    jwt added

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path , include
 from .views import *
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/login/', CustomTokenObtainPairView.as_view(), name='token-login'),
     path('token/logout/', CustomTokenDestroyView.as_view(), name='token-logout'),
+    path('jwt/create/',  TokenObtainPairView.as_view(), name='jwt-create'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='jwt-verify'),
+    path('jwt/refresh/',  TokenRefreshView.as_view(), name='jwt-refresh'),
 ]
